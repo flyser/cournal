0ded7609362af4682a32c7fbf18161cf1b60de7f
Fabian Henze <flyser42@gmx.de>
2012-05-22 12:14:06 +0200

    Move DrawingArea implementation in its own class

--- cournal.py
+++ cournal.py
@@ -27,7 +27,8 @@ from gi.repository import Gtk, Poppler, Gdk
 import cairo
 
 class Page():
-    def __init__(self, page):
+    def __init__(self, document, page):
+        self.document = document
         self.pdf = page
         self.width, self.height = page.get_size()
 
@@ -39,55 +40,31 @@ class Document():
                                                   None)
         self.pages = list()
         for i in range(self.pdf.get_n_pages()):
-            page = Page(self.pdf.get_page(i))
+            page = Page(self, self.pdf.get_page(i))
             self.pages.append(page)
         print("The document has " + str(len(self.pages)) + " pages")
 
-class MyWindow(Gtk.Window):
+class MyDrawingArea(Gtk.DrawingArea):
     def __init__(self, **args):
-        Gtk.Window.__init__(self, **args)
-        self.set_default_size(width=700, height=800)
-        
+        Gtk.DrawingArea.__init__(self, **args)
         self.backbuffer = None
         self.lastpoint = None
 
-        # Bob the builder
-        builder = Gtk.Builder()
-        builder.add_from_file("mainwindow.glade")
-        self.add(builder.get_object("outer_box"))
+        self.set_double_buffered(False)
+        self.set_size_request(width=100, height=800)
+        self.set_events(Gdk.EventMask.BUTTON_PRESS_MASK |
+                        Gdk.EventMask.BUTTON_RELEASE_MASK |
+                        Gdk.EventMask.POINTER_MOTION_MASK)
+#                       Gdk.EventMask.POINTER_MOTION_HINT_MASK)
         
-        # Give interesting widgets names:
-        self.viewport = builder.get_object("viewport")
-        self.drawingarea = builder.get_object("drawingarea")
-        
-        # Initialize the drawing area:
-        self.drawingarea.set_double_buffered(False)
-        self.drawingarea.set_size_request(width=100, height=800)
-        self.drawingarea.set_events(Gdk.EventMask.BUTTON_PRESS_MASK |
-                                    Gdk.EventMask.BUTTON_RELEASE_MASK |
-                                    Gdk.EventMask.POINTER_MOTION_MASK)
-#                                    Gdk.EventMask.POINTER_MOTION_HINT_MASK)
-        
-        self.drawingarea.connect("draw", self.draw_drawingarea)
-        self.drawingarea.connect("configure_event", self.configure_drawingarea)
-        self.drawingarea.connect("motion_notify_event", self.motion_drawingarea)
-        self.drawingarea.connect("button-press-event", self.press_drawingarea)
-        self.drawingarea.connect("button-release-event", self.release_drawingarea)
-        
-    def set_cursor(self):
-        width, height = 4, 4
-        
-        s = cairo.ImageSurface(cairo.FORMAT_A1, width, height)
-        context = cairo.Context(s)
-        context.set_source_rgb(0,0,0)
-        context.paint()
-        
-        cursor_pixbuf = Gdk.pixbuf_get_from_surface(s, 0, 0, width, height)
-        cursor = Gdk.Cursor.new_from_pixbuf(Gdk.Display.get_default(),
-                                            cursor_pixbuf, width/2, height/2)
-        self.drawingarea.get_window().set_cursor(cursor)
-        
-    def press_drawingarea(self, widget, event):
+        self.connect("draw", self.draw)
+        self.connect("configure_event", self.configure)
+        self.connect("realize", self.set_cursor)
+        self.connect("motion_notify_event", self.motion)
+        self.connect("button-press-event", self.press)
+        self.connect("button-release-event", self.release)
+    
+    def press(self, widget, event):
         if event.button != 1:
             return
         #print("Press " + str((event.x,event.y)))
@@ -97,7 +74,7 @@ class MyWindow(Gtk.Window):
         print(currentPage.width, "x", currentPage.height)
         currentPage.strokes.append([event.x*currentPage.width/actualWidth, event.y*currentPage.width/actualWidth])
         
-    def release_drawingarea(self, widget, event):
+    def release(self, widget, event):
         if self.lastpoint is None:
             return
         if event.button != 1:
@@ -108,7 +85,7 @@ class MyWindow(Gtk.Window):
         
         self.lastpoint = None
         
-    def motion_drawingarea(self, widget, event):
+    def motion(self, widget, event):
         if self.lastpoint is None:
             return
         #print("\rMotion "+str((event.x,event.y))+"  ", end="")
@@ -135,7 +112,20 @@ class MyWindow(Gtk.Window):
         self.lastpoint = [event.x, event.y]
         currentPage.strokes[-1].extend([event.x*currentPage.width/actualWidth, event.y*currentPage.width/actualWidth])
         
-    def configure_drawingarea(self, widget, event):
+    def set_cursor(self, widget):
+        width, height = 4, 4
+        
+        s = cairo.ImageSurface(cairo.FORMAT_A1, width, height)
+        context = cairo.Context(s)
+        context.set_source_rgb(0,0,0)
+        context.paint()
+        
+        cursor_pixbuf = Gdk.pixbuf_get_from_surface(s, 0, 0, width, height)
+        cursor = Gdk.Cursor.new_from_pixbuf(Gdk.Display.get_default(),
+                                            cursor_pixbuf, width/2, height/2)
+        self.get_window().set_cursor(cursor)
+
+    def configure(self, widget, event):
         #print("Configure " + str((event.width, event.height)))
         actualWidth = widget.get_allocation().width
         actualHeight = widget.get_allocation().height
@@ -162,21 +152,39 @@ class MyWindow(Gtk.Window):
                 context.line_to(stroke[2*i+2], stroke[2*i+3])
             context.stroke()
         
-    def draw_drawingarea(self, widget, context):
+    def draw(self, widget, context):
         #print("Draw")
         # you can ignore the width=100 here. it has no effect
-        self.drawingarea.set_size_request(width=100, height=self.backbuffer.get_height())
+        self.set_size_request(width=100, height=self.backbuffer.get_height())
         self.drawingareaContext = context
         
         context.set_source_surface(self.backbuffer, 0, 0)
         context.paint()
 
-
+class MyWindow(Gtk.Window):
+    def __init__(self, **args):
+        Gtk.Window.__init__(self, **args)
+        self.set_default_size(width=700, height=800)
+        
+        # Bob the builder
+        builder = Gtk.Builder()
+        builder.add_from_file("mainwindow.glade")
+        self.add(builder.get_object("outer_box"))
+        
+        # Give interesting widgets names:
+        self.viewport = Gtk.VBox()
+        builder.get_object("viewport").add(self.viewport)
+        self.drawingarea = MyDrawingArea()
+        self.foobar = Gtk.Button()
+        self.viewport.add(self.drawingarea)
+        self.viewport.add(self.foobar)
+        
+        
 document = Document(sys.argv[1])
 currentPage = document.pages[0]
 
 window = MyWindow(title="Cournal")
 window.connect("delete-event", Gtk.main_quit)
 window.show_all()
-window.set_cursor()
+#window.set_cursor()
 Gtk.main()
