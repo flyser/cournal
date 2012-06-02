00fb8890c1fd61c3c0d17f3d4b627e55b02798d3
Fabian Henze <flyser42@gmx.de>
2012-06-02 13:18:24 +0200

    Remove unused import

--- cournal/aboutdialog.py
+++ cournal/aboutdialog.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Cournal.  If not, see <http://www.gnu.org/licenses/>.
 
-import cairo
 from gi.repository import Gtk
 
 class AboutDialog(Gtk.AboutDialog):
