2ed9074d3008ad81fbfe6196b7d00b823c4d9569
Fabian Henze <flyser42@gmx.de>
2012-06-07 22:11:33 +0200

    Fix line break

--- xojtools/__init__.py
+++ xojtools/__init__.py
@@ -3,5 +3,4 @@ from .page import Page
 from .stroke import Stroke
 from .textbox import TextBox
 
-__all__ = ["Layer", "Page", "Stroke", "TextBox",
-           "xournalparser"]
+__all__ = ["Layer", "Page", "Stroke", "TextBox", "xournalparser"]
