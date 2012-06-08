flyser/cournal

commit 9268290330ae9ee6efb58688966c476a8eca0387

Huge amount of cleanups and an 'Import' button in addition to 'Open'
These changes aim to reduce ambiguity in function and variable naming,
as well as generally improving code structure and readability.

cournal/document/document.py
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Cournal.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-import gzip
+from os.path import abspath
+from gzip import open as open_xoj
 
 from gi.repository import Poppler, GLib
 import cairo
@@ -27,7 +27,7 @@ from . import Page
 
 class Document:
     def __init__(self, pdfname):
-        self.pdfname = os.path.abspath(pdfname)
+        self.pdfname = abspath(pdfname)
         uri = GLib.filename_to_uri(self.pdfname, None)
         self.pdf = Poppler.Document.new_from_file(uri, None)
         self.width = 0
@@ -52,7 +52,7 @@ class Document:
     def clear_pages(self):
         for page in self.pages:
             for stroke in page.layers[0].strokes[:]:
-                page.delete_stroke_with_coords_callback(stroke.coords)
+                page.delete_stroke(stroke)
     
     def export_pdf(self, filename):
         try:
@@ -68,13 +68,6 @@ class Document:
             
             page.pdf.render_for_printing(context)
             
-            # Render all strokes
-            context.set_source_rgb(0,0,0.4)
-            context.set_antialias(cairo.ANTIALIAS_GRAY)
-            context.set_line_cap(cairo.LINE_CAP_ROUND)
-            context.set_line_join(cairo.LINE_JOIN_ROUND)
-            context.set_line_width(1.5)
-
             for stroke in page.layers[0].strokes:
                 stroke.draw(context)
             
@@ -83,7 +76,7 @@ class Document:
     def save_xoj_file(self, filename):
         pagenum = 1
         try:
-            f = gzip.open(filename, "wb")
+            f = open_xoj(filename, "wb")
         except IOError as ex:
             print("Error saving document:", ex)
             #FIXME: Move error handler to mainwindow.py and show error message

cournal-server.py
@@ -100,7 +100,7 @@ class Document(pb.Viewable):
         self.users.append(user)
         for pagenum in range(len(self.pages)):
             for stroke in self.pages[pagenum].strokes:
-                user.remote.callRemote("add_stroke", pagenum, stroke)
+                user.remote.callRemote("new_stroke", pagenum, stroke)
     
     def removeUser(self, user):
         self.users.remove(user)
@@ -121,7 +121,7 @@ class Document(pb.Viewable):
         self.pages[pagenum].strokes.append(stroke)
         
         debug(3, "New stroke on page", pagenum+1)
-        self.broadcast("add_stroke", pagenum, stroke, except_user=from_user)
+        self.broadcast("new_stroke", pagenum, stroke, except_user=from_user)
         
     def view_delete_stroke_with_coords(self, from_user, pagenum, coords):
         for stroke in self.pages[pagenum].strokes:

cournal/connectiondialog.py
@@ -50,7 +50,7 @@ class ConnectionDialog(Gtk.Dialog):
         
         self.show_all()
     
-    def response_cb(self, widget, response_id):
+    def response(self, widget, response_id):
         if response_id != Gtk.ResponseType.ACCEPT:
             self.destroy()
             return
@@ -68,6 +68,10 @@ class ConnectionDialog(Gtk.Dialog):
                 return
             self.parent.document.clear_pages()
             
+        self.new_connection(server, port)
+            
+    
+    def new_connection(self, server, port):
         network.set_document(self.parent.document)
         d = network.connect(server, port)
         d.addCallbacks(self.on_connected, self.on_connection_failure)
@@ -77,6 +81,7 @@ class ConnectionDialog(Gtk.Dialog):
         self.error_label.set_text("")
         self.connecting_label.set_text("Connecting to {} ...".format(server))
         self.get_action_area().set_sensitive(False)
+        
     
     def confirm_clear_document(self):
         message = Gtk.MessageDialog(self, (Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT), Gtk.MessageType.WARNING, Gtk.ButtonsType.YES_NO, "Close current document?" )
@@ -101,7 +106,7 @@ class ConnectionDialog(Gtk.Dialog):
         self.get_action_area().set_sensitive(True)
 
     def run_nonblocking(self):
-        self.connect('response', self.response_cb)
+        self.connect('response', self.response)
         self.show()
 
 class ServerPortEntry(Gtk.EventBox):

cournal/document/page.py
@@ -17,7 +17,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Cournal.  If not, see <http://www.gnu.org/licenses/>.
 
-from . import Layer
+from math import sqrt
+
+from . import Layer, Stroke
+from .. import network
 
 class Page:
     def __init__(self, document, pdf, number, layers=None):
@@ -28,27 +31,42 @@ class Page:
         if self.layers is None:
             self.layers = [Layer(self, 0)]
         
+        self.widget = None
         self.width, self.height = pdf.get_size()
-        
-        self.new_stroke_callbacks = []
-        self.delete_stroke_callbacks = []
     
-    #FIXME: Move to layer
-    def add_new_stroke_callback(self, callback):
-        self.new_stroke_callbacks.append(callback)
-       
-    def add_delete_stroke_callback(self, callback):
-        self.delete_stroke_callbacks.append(callback)
-
-    def new_stroke_callback(self, stroke):
+    def new_stroke(self, stroke, send_to_network=False):
         self.layers[0].strokes.append(stroke)
         stroke.layer = self.layers[0]
-        for callback in self.new_stroke_callbacks:
-            callback(stroke)
-       
-    def delete_stroke_with_coords_callback(self, coords):
-        for stroke in self.layers[0].strokes:
+        if self.widget:
+            self.widget.draw_remote_stroke(stroke)
+        if send_to_network:
+            network.new_stroke(self.number, stroke)
+        
+    def new_unfinished_stroke(self, color, linewidth):
+        return Stroke(layer=self.layers[0], color=color, linewidth=linewidth, coords=[])
+    
+    def finish_stroke(self, stroke):
+        #TODO: rerender portion of screen.
+        network.new_stroke(self.number, stroke)
+
+    def delete_stroke_with_coords(self, coords):
+        for stroke in self.layers[0].strokes[:]:
             if stroke.coords == coords:
-                self.layers[0].strokes.remove(stroke)
-                for callback in self.delete_stroke_callbacks:
-                    callback(stroke)
+                self.delete_stroke(stroke, send_to_network=False)
+    
+    def delete_stroke(self, stroke, send_to_network=False):
+        self.layers[0].strokes.remove(stroke)
+        if self.widget:
+            self.widget.delete_remote_stroke(stroke)
+        if send_to_network:
+            network.delete_stroke_with_coords(self.number, stroke.coords)
+    
+    def get_strokes_near(self, x, y, radius):
+        for stroke in self.layers[0].strokes[:]:
+            for coord in stroke.coords:
+                s_x = coord[0]
+                s_y = coord[1]
+                if sqrt((s_x-x)**2 + (s_y-y)**2) < radius:
+                    yield stroke
+                    break
+    

cournal/document/stroke.py
@@ -22,10 +22,10 @@ import cairo
 from twisted.spread import pb
 
 class Stroke(pb.Copyable, pb.RemoteCopy):
-    def __init__(self, layer, color, width, coords=None):
+    def __init__(self, layer, color, linewidth, coords=None):
         self.layer = layer
         self.color = color
-        self.width = width
+        self.linewidth = linewidth
         self.coords = coords
         if self.coords is None:
             self.coords = []
@@ -35,19 +35,18 @@ class Stroke(pb.Copyable, pb.RemoteCopy):
         d = dict()
         d["color"] = self.color
         d["coords"] = self.coords
-        d["width"] = self.width
+        d["linewidth"] = self.linewidth
         return d
 
     def draw(self, context, scaling=1):
         context.save()
         r, g, b, opacity = self.color
-        linewidth = self.width
         
         context.set_source_rgba(r/255, g/255, b/255, opacity/255)
         context.set_antialias(cairo.ANTIALIAS_GRAY)
         context.set_line_join(cairo.LINE_JOIN_ROUND)
         context.set_line_cap(cairo.LINE_CAP_ROUND)
-        context.set_line_width(linewidth)
+        context.set_line_width(self.linewidth)
         
         first = self.coords[0]
         context.move_to(first[0], first[1])

cournal/document/xojparser.py
@@ -23,8 +23,7 @@ from gzip import open as open_xoj
 
 import xml.etree.cElementTree as ET
 
-from . import Document, Page, Layer, Stroke
-from .. import network
+from . import Document, Stroke
 
 """A simplified parser for Xournal files using the ElementTree API."""
 
@@ -60,9 +59,7 @@ def import_into_document(document, filename, window):
         strokes = pages[p].findall("layer/stroke")
         for s in range(len(strokes)):
             stroke = _parse_stroke(strokes[s], document.pages[p].layers[0])
-            document.pages[p].layers[0].strokes.append(stroke)
-            if network.is_connected:
-                network.local_new_stroke(p, stroke)    
+            document.pages[p].new_stroke(stroke, send_to_network=True)
     return document
 
 def _parse_stroke(stroke, layer):
@@ -91,8 +88,12 @@ def _parse_stroke(stroke, layer):
             coordinates.append([x, y, width])
         else:
             coordinates.append([x, y])
+    
+    # If the stroke is just a point, Xournal saves the same coordinates twice
+    if len(coordinates) == 2 and coordinates[0] == coordinates[1]:
+        del coordinates[1]
 
-    return Stroke(layer, color=color, width=nominalWidth, coords=coordinates)
+    return Stroke(layer, color=color, linewidth=nominalWidth, coords=coordinates)
 
 def _get_background(tree):
     """Gets the background pdf file name of a xournal document"""

cournal/mainwindow.py
@@ -24,6 +24,11 @@ from .viewer import Layout
 from .document import Document, xojparser
 from . import ConnectionDialog, AboutDialog
 
+pdf_filter = Gtk.FileFilter()
+pdf_filter.add_mime_type("application/pdf")
+xoj_filter = Gtk.FileFilter()
+xoj_filter.add_mime_type("application/x-xoj")
+
 class MainWindow(Gtk.Window):
     def __init__(self, **args):
         Gtk.Window.__init__(self, title="Cournal", **args)
@@ -38,38 +43,51 @@ class MainWindow(Gtk.Window):
         builder.add_from_file("mainwindow.glade")
         self.add(builder.get_object("outer_box"))
         
-        # Initialize the main pdf viewer layout
+        # Initialize the main journal layout
         self.layout = None
         self.scrolledwindow = builder.get_object("scrolledwindow")
         
         # Menu Bar:
-        self.open_pdf = builder.get_object("imagemenuitem_open_pdf")
-        self.connectbutton = builder.get_object("imagemenuitem_connect")
-        self.open_xojbutton = builder.get_object("imagemenuitem_open_xoj")
-        self.savebutton = builder.get_object("imagemenuitem_save")
-        self.exportbutton = builder.get_object("imagemenuitem_export_pdf")
-        self.aboutbutton = builder.get_object("imagemenuitem_about")
-        self.quitbutton = builder.get_object("imagemenuitem_quit")
+        self.menu_open_xoj = builder.get_object("imagemenuitem_open_xoj")
+        self.menu_open_pdf = builder.get_object("imagemenuitem_open_pdf")
+        self.menu_connect = builder.get_object("imagemenuitem_connect")
+        self.menu_save = builder.get_object("imagemenuitem_save")
+        self.menu_export_pdf = builder.get_object("imagemenuitem_export_pdf")
+        self.menu_import_xoj = builder.get_object("imagemenuitem_import_xoj")
+        self.menu_quit = builder.get_object("imagemenuitem_quit")
+        self.menu_about = builder.get_object("imagemenuitem_about")
 
-        self.connectbutton.set_sensitive(False)
-        self.savebutton.set_sensitive(False)
-        self.exportbutton.set_sensitive(False)
-        self.open_xojbutton.set_sensitive(False)
+        self.menu_connect.set_sensitive(False)
+        self.menu_save.set_sensitive(False)
+        self.menu_export_pdf.set_sensitive(False)
+        self.menu_import_xoj.set_sensitive(False)
         
-        self.open_pdf.connect("activate", self.on_open_pdf_click)
-        self.connectbutton.connect("activate", self.on_connect_click)
-        self.open_xojbutton.connect("activate", self.on_open_xoj_click)
-        self.savebutton.connect("activate", self.on_save_click)
-        self.exportbutton.connect("activate", self.on_export_click)
-        self.aboutbutton.connect("activate", self.on_about_click)
-        self.quitbutton.connect("activate", lambda _: self.destroy())
+        self.menu_open_xoj.connect("activate", self.run_open_xoj_dialog)
+        self.menu_open_pdf.connect("activate", self.run_open_pdf_dialog)
+        self.menu_connect.connect("activate", self.run_connection_dialog)
+        self.menu_save.connect("activate", self.run_save_dialog)
+        self.menu_export_pdf.connect("activate", self.run_export_pdf_dialog)
+        self.menu_import_xoj.connect("activate", self.run_import_xoj_dialog)
+        self.menu_quit.connect("activate", lambda _: self.destroy())
+        self.menu_about.connect("activate", self.run_about_dialog)
     
-    def on_open_pdf_click(self, menuitem):
+    def _set_document(self, document):
+        self.document = document
+        for child in self.scrolledwindow.get_children():
+            self.scrolledwindow.remove(child)
+        self.layout = Layout(self.document)
+        self.scrolledwindow.add(self.layout)
+        self.scrolledwindow.show_all()
+        
+        self.menu_connect.set_sensitive(True)
+        self.menu_save.set_sensitive(True)
+        self.menu_export_pdf.set_sensitive(True)
+        self.menu_import_xoj.set_sensitive(True)
+    
+    def run_open_pdf_dialog(self, menuitem):
         dialog = Gtk.FileChooserDialog("Open File", self, Gtk.FileChooserAction.OPEN,
                                        (Gtk.STOCK_OPEN, Gtk.ResponseType.ACCEPT,
                                         Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
-        pdf_filter = Gtk.FileFilter()
-        pdf_filter.add_mime_type("application/pdf")
         dialog.set_filter(pdf_filter)
         
         if dialog.run() == Gtk.ResponseType.ACCEPT:
@@ -78,104 +96,84 @@ class MainWindow(Gtk.Window):
             try:
                 document = Document(filename)
             except GError as ex:
-                message = Gtk.MessageDialog(self, (Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT), Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, "Unable to open PDF" )
-                message.format_secondary_text(ex)
-                message.set_title("Error")
-                message.connect("response", lambda _,x: message.destroy())
-                message.show()
-                print("Unable to open PDF file:", ex)
+                self.run_error_dialog("Unable to open PDF", ex)
                 dialog.destroy()
                 return
-            
-            self.document = document
-            for child in self.scrolledwindow.get_children():
-                self.scrolledwindow.remove(child)
-            self.layout = Layout(self.document)
-            self.scrolledwindow.add(self.layout)
-            self.scrolledwindow.show_all()
-            
-            self.connectbutton.set_sensitive(True)
-            self.open_xojbutton.set_sensitive(True)
-            self.savebutton.set_sensitive(True)
-            self.exportbutton.set_sensitive(True)
-
+            self._set_document(document)
         dialog.destroy()
-        
-    def on_connect_click(self, menuitem):
+    
+    def run_connection_dialog(self, menuitem):
+        def destroyed(widget):
+            self._connection_dialog = None
         # Need to hold a reference, so the object does not get garbage collected
         self._connection_dialog = ConnectionDialog(self)
-        self._connection_dialog.connect("destroy", self.connection_dialog_destroyed)
+        self._connection_dialog.connect("destroy", destroyed)
         self._connection_dialog.run_nonblocking()
-
-    def on_open_xoj_click(self, menuitem):
+        
+    def run_import_xoj_dialog(self, menuitem):
         dialog = Gtk.FileChooserDialog("Open File", self, Gtk.FileChooserAction.OPEN,
                                        (Gtk.STOCK_OPEN, Gtk.ResponseType.ACCEPT,
                                         Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
-        xoj_filter = Gtk.FileFilter()
-        xoj_filter.add_mime_type("application/x-xoj")
         dialog.set_filter(xoj_filter)
 
         if dialog.run() == Gtk.ResponseType.ACCEPT:
             filename = dialog.get_filename()
-            document = xojparser.import_into_document(self.document, filename, self)
-#            try:
-#            except Exception as ex:
-#                print(ex)
-#                dialog.destroy()
-#                return
-#            
-#            self.document = document
-#            for child in self.scrolledwindow.get_children():
-#                self.scrolledwindow.remove(child)
-#            self.layout = Layout(self.document)
-#            self.scrolledwindow.add(self.layout)
-#            self.scrolledwindow.show_all()
-#            
-#            self.connectbutton.set_sensitive(True)
-#            #self.open_xojbutton.set_sensitive(True)
-#            self.savebutton.set_sensitive(True)
-#            self.exportbutton.set_sensitive(True)
-
+            xojparser.import_into_document(self.document, filename, self)
         dialog.destroy()
     
-    def on_save_click(self, menuitem):
+    def run_open_xoj_dialog(self, menuitem):
+        dialog = Gtk.FileChooserDialog("Open File", self, Gtk.FileChooserAction.OPEN,
+                                       (Gtk.STOCK_OPEN, Gtk.ResponseType.ACCEPT,
+                                        Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
+        dialog.set_filter(xoj_filter)
+
+        if dialog.run() == Gtk.ResponseType.ACCEPT:
+            filename = dialog.get_filename()
+            try:
+                document = xojparser.new_document(filename, self)
+            except Exception as ex:
+                print(ex)
+                dialog.destroy()
+                return
+            self._set_document(document)
+        dialog.destroy()
+
+    def run_save_dialog(self, menuitem):
         dialog = Gtk.FileChooserDialog("Save File", self, Gtk.FileChooserAction.SAVE,
                                        (Gtk.STOCK_SAVE, Gtk.ResponseType.ACCEPT,
                                         Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
-        dialog.set_current_name("document.xoj")
-        
-        xoj_filter = Gtk.FileFilter()
-        xoj_filter.add_mime_type("application/x-xoj")
         dialog.set_filter(xoj_filter)
+        dialog.set_current_name("document.xoj")
 
         if dialog.run() == Gtk.ResponseType.ACCEPT:
             filename = dialog.get_filename()
             self.document.save_xoj_file(filename)
         dialog.destroy()
 
-    def on_export_click(self, menuitem):
+    def run_export_pdf_dialog(self, menuitem):
         dialog = Gtk.FileChooserDialog("Export PDF", self, Gtk.FileChooserAction.SAVE,
                                        (Gtk.STOCK_SAVE, Gtk.ResponseType.ACCEPT,
                                         Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
-        dialog.set_current_name("annotated_document.pdf")
-        
-        pdf_filter = Gtk.FileFilter()
-        pdf_filter.add_mime_type("application/pdf")
         dialog.set_filter(pdf_filter)
+        dialog.set_current_name("annotated_document.pdf")
         
         if dialog.run() == Gtk.ResponseType.ACCEPT:
             filename = dialog.get_filename()
             self.document.export_pdf(filename)
         dialog.destroy()
         
-    def on_about_click(self, menuitem):
+    def run_about_dialog(self, menuitem):
+        def destroyed(widget):
+            self._about_dialog = None
         # Need to hold a reference, so the object does not get garbage collected
         self._about_dialog = AboutDialog(self)
-        self._about_dialog.connect("destroy", self.about_dialog_destroyed)
+        self._about_dialog.connect("destroy", destroyed)
         self._about_dialog.run_nonblocking()
     
-    def about_dialog_destroyed(self, widget):
-        self._about_dialog = None
-        
-    def connection_dialog_destroyed(self, widget):
-        self._connection_dialog = None
+    def run_error_dialog(self, first, second):
+        print("Unable to open PDF file:", second)
+        message = Gtk.MessageDialog(self, (Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT), Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, first)
+        message.format_secondary_text(second)
+        message.set_title("Error")
+        message.connect("response", lambda _,x: message.destroy())
+        message.show()        

cournal/network.py
@@ -58,7 +58,7 @@ class Network(pb.Referenceable):
         self.is_connected = True
         self.perspective = perspective
         d = perspective.callRemote("joinDocument", "document1")
-        d.addCallbacks(self.gotDocument, callbackArgs=["document1"])
+        d.addCallbacks(self.got_server_document, callbackArgs=["document1"])
         
         return d
 
@@ -68,23 +68,29 @@ class Network(pb.Referenceable):
         
         return reason
         
-    def gotDocument(self, document, name):
+    def got_server_document(self, server_document, name):
         debug(2, "Started editing", name)
-        self.remote_doc = document
+        self.server_document = server_document
 
-    def remote_add_stroke(self, pagenum, stroke):
+    def remote_new_stroke(self, pagenum, stroke):
+        """Called by the server"""
         if self.document and pagenum < len(self.document.pages):
-            self.document.pages[pagenum].new_stroke_callback(stroke)
+            self.document.pages[pagenum].new_stroke(stroke)
     
-    def local_new_stroke(self, pagenum, stroke):
-        d = self.remote_doc.callRemote("new_stroke", pagenum, stroke)
+    def new_stroke(self, pagenum, stroke):
+        """Called by local code"""
+        if self.is_connected:
+            self.server_document.callRemote("new_stroke", pagenum, stroke)
 
     def remote_delete_stroke_with_coords(self, pagenum, coords):
+        """Called by the server"""
         if self.document and pagenum < len(self.document.pages):
-            self.document.pages[pagenum].delete_stroke_with_coords_callback(coords)
+            self.document.pages[pagenum].delete_stroke_with_coords(coords)
     
-    def local_delete_stroke_with_coords(self, pagenum, coords):
-        d = self.remote_doc.callRemote("delete_stroke_with_coords", pagenum, coords)
+    def delete_stroke_with_coords(self, pagenum, coords):
+        """Called by local code"""
+        if self.is_connected:
+            self.server_document.callRemote("delete_stroke_with_coords", pagenum, coords)
 
     def shutdown(self, result):
         reactor.stop()

commit dd5761167497ccfa1697f128a2dbb799618a2693

Fix saving, which was broken since 9268290330ae9ee6efb58688966c476a8eca0387

cournal/document/document.py
@@ -101,7 +101,7 @@ class Document:
                 r += "<layer>\n"
                 for stroke in layer.strokes:
                     red, g, b, opacity = stroke.color
-                    r += "<stroke tool=\"pen\" color=\"#{:02X}{:02X}{:02X}{:02X}\" width=\"{}\">\n".format(red, g, b, opacity, stroke.width)
+                    r += "<stroke tool=\"pen\" color=\"#{:02X}{:02X}{:02X}{:02X}\" width=\"{}\">\n".format(red, g, b, opacity, stroke.linewidth)
                     first = stroke.coords[0]
                     for coord in stroke.coords:
                         r += " {} {}".format(coord[0], coord[1])