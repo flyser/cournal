flyser/cournal

commit cd375c3428f2882879f6698317ff66e3971546b5

Localization support and german translation

cournal/server/server.py
@@ -84,13 +84,13 @@ class CournalServer:
         if not os.path.isdir(self.autosave_directory):
             # Only create the autosave directory, if it wasn't changed by the user
             if self.autosave_directory != DEFAULT_AUTOSAVE_DIRECTORY:
-                print("Autosave directory '{}' does not exist.", file=sys.stderr)
+                print(_("Autosave directory '{}' does not exist.").format(self.autosave_directory), file=sys.stderr)
                 raise Exception()
             else:
                 try:
                     os.mkdir(self.autosave_directory)
                 except Exception as ex:
-                    print("Could not create autosave directory: {}".format(self.autosave_directory, ex), file=sys.stderr)
+                    print(_("Could not create autosave directory: {}").format(self.autosave_directory, ex), file=sys.stderr)
                     raise ex
         os.chdir(self.autosave_directory)
 
@@ -103,7 +103,7 @@ class CournalServer:
                 self.documents[name] = Document(name)
                 self.documents[name].pages = pickle.load(file)
         
-        debug(1, "Loaded {} documents".format(len(self.documents)))
+        debug(1, _("Loaded {} documents").format(len(self.documents)))
         
         reactor.callLater(self.autosave_interval, self.save_documents)
     
@@ -117,19 +117,21 @@ class CournalServer:
             with open(lockfile, "rb") as f:
                 pid = int(f.read())
             if self.is_pid_dead(pid):
-                print("The autosave directory was locked by another instance of cournal-server,", file=sys.stderr)
-                print("that is not running anymore. This happens, when cournal-server crashed.", file=sys.stderr)
-                print("Make sure that no server is using the autosave directory '{}',".format(self.autosave_directory), file=sys.stderr)
-                print("delete '{}' and try again.".format(lockfile), file=sys.stderr)
+                print(_(\
+"""The autosave directory was locked by another instance of cournal-server,
+that is not running anymore. This happens, when cournal-server crashed.
+Make sure that no server is using the autosave directory '{}',
+delete '{}' and try again.""").format(self.autosave_directory, lockfile), file=sys.stderr)
             else:
-                print("The autosave directory is locked by another instance of cournal-server.", file=sys.stderr)
-                print("To run multiple instances concurrently, you need to set a different autosave directory and port.", file=sys.stderr)
+                print(_(\
+"""The autosave directory is locked by another instance of cournal-server.
+To run multiple instances concurrently, you need to set a different autosave directory and port."""), file=sys.stderr)
             sys.exit(-1)
         else:
             try:
                 open(lockfile, 'w').write(str(os.getpid()))
             except Exception as ex:
-                print("Could not create file in autosave directory: {}".format(ex), file=sys.stderr)
+                print(_("Could not create file in autosave directory: {}").format(ex), file=sys.stderr)
                 raise ex
         self.lockfile = lockfile
     
@@ -169,7 +171,7 @@ class CournalServer:
         
         The on-disk format is a pickled list of pages.
         """
-        debug(3, "Saving all documents.")
+        debug(3, _("Saving all documents."))
         for name, document in self.documents.items():
             if not document.has_unsaved_changes:
                 continue
@@ -241,7 +243,7 @@ class User(pb.Avatar):
         name -- Name of the user
         server -- A CournalServer object 
         """
-        debug(1, "New User connected:", name)
+        debug(1, _("New User connected: {}").format(name))
         self.name = name
         self.server = server
         self.remote = None
@@ -249,7 +251,7 @@ class User(pb.Avatar):
         
     def __del__(self):
         """Destructor. Called when the user disconnects."""
-        debug(1, "User disconnected:", self.name)
+        debug(1, _("User disconnected: {}").format(self.name))
         
     def attached(self, mind):
         """
@@ -277,7 +279,7 @@ class User(pb.Avatar):
         """
         Return a list of all our documents.
         """
-        debug(2, "User", self.name, "requested document list")
+        debug(2, _("User {} requested document list").format(self.name))
         
         return list(self.server.documents.keys())
     
@@ -288,7 +290,7 @@ class User(pb.Avatar):
         Positional arguments:
         documentname -- Name of the requested document session
         """
-        debug(2, "User", self.name, "started editing", documentname)
+        debug(2, _("User {} started editing {}").format(self.name, documentname))
         
         document = self.server.get_document(documentname)
         if isinstance(document, Failure):
@@ -381,7 +383,7 @@ class Document(pb.Viewable):
             self.pages.append(Page())
         self.pages[pagenum].strokes.append(stroke)
         
-        debug(3, "New stroke on page", pagenum+1)
+        debug(3, _("New stroke on page {}").format(pagenum + 1))
         self.broadcast("new_stroke", pagenum, stroke, except_user=from_user)
         
     def view_delete_stroke_with_coords(self, from_user, pagenum, coords):
@@ -400,7 +402,7 @@ class Document(pb.Viewable):
             if stroke.coords == coords:
                 self.pages[pagenum].strokes.remove(stroke)
                 
-                debug(3, "Deleted stroke on page", pagenum+1)
+                debug(3, _("Deleted stroke on page {}").format(pagenum + 1))
                 self.broadcast("delete_stroke_with_coords", pagenum, coords, except_user=from_user)
 
 class CmdlineParser():
@@ -417,14 +419,14 @@ class CmdlineParser():
         """
         Parse commandline options.
         """
-        parser = argparse.ArgumentParser(description="Server for Cournal.",
-                                         epilog="e.g.: %(prog)s -p port")
+        parser = argparse.ArgumentParser(description=_("Server for Cournal."),
+                                         epilog=_("e.g.: %(prog)s -p port"))
         parser.add_argument("-p", "--port", nargs=1, type=int, default=[self.port],
-                            help="Port to listen on")
+                            help=_("Port to listen on"))
         parser.add_argument("-s", "--autosave-directory", nargs=1, default=[self.autosave_directory],
-                            help="The directory within which to store the documents on the server.")
+                            help=_("The directory within which to store the documents on the server."))
         parser.add_argument("-i", "--autosave-interval", nargs=1, type=int, default=[self.autosave_interval],
-                            help="Interval in seconds within which to save modified documents to permanent storage. Set to 0 to disable autosave.")
+                            help=_("Interval in seconds within which to save modified documents to permanent storage. Set to 0 to disable autosave."))
         parser.add_argument("-v", "--version", action="version",
                             version="%(prog)s " + cournal_version)
         args = parser.parse_args()
@@ -497,9 +499,9 @@ def main():
     try:
         reactor.listenTCP(port, pb.PBServerFactory(p))
     except CannotListenError as err:
-        debug(0, "ERROR: Failed to listen on port", err.port)
+        debug(0, _("ERROR: Failed to listen on port {}").format(err.port))
         return 1
-    debug(2, "Listening on port", port)
+    debug(2, _("Listening on port {}").format(port))
 
     reactor.run()
 

.gitignore
@@ -1,16 +1,28 @@
+# Editors
 *~
 *.bak
 *.kate-swp
+*.swp
+
+# Python
 __pycache__
 *.pyc
 *.pyo
+
+# Documents
 *.xoj
 *.pdf
-*.swp
 
+# Translation:
+*.h
+*.mo
+*.pot
+
+# Twisted
 cournal/twisted
 t3k
 
+# Packaging
 build
 dist
 MANIFEST

cournal/aboutdialog.py
@@ -39,7 +39,7 @@ class AboutDialog(Gtk.AboutDialog):
         self.set_program_name("Cournal")
         self.set_logo_icon_name("cournal")
         self.set_copyright("© Fabian Henze")
-        self.set_comments("A collaborative note taking and journal application using a stylus.")
+        self.set_comments(_("A collaborative note taking and journal application using a stylus."))
         self.set_license_type(Gtk.License.GPL_3_0)
         self.set_website("http://cournal-project.org")
         self.set_authors(["Fabian Henze", "Simon Vetter", "Martin Grohmann"])

cournal/connectiondialog/connectiondialog.py
@@ -39,6 +39,7 @@ class ConnectionDialog(Gtk.Dialog):
         self.parent = parent
         
         builder = Gtk.Builder()
+        builder.set_translation_domain("cournal")
         builder.add_from_file(cournal.__path__[0] + "/connection_dialog.glade")
         grid = builder.get_object("grid_main")
         
@@ -56,7 +57,7 @@ class ConnectionDialog(Gtk.Dialog):
         self.set_modal(False)
         self.set_has_resize_grip(False)
         self.set_resizable(False)
-        self.set_title("Connect to Server")
+        self.set_title(_("Connect to Server"))
         self.set_transient_for(parent)
         self.add_button(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
         self.add_button(Gtk.STOCK_CONNECT, Gtk.ResponseType.ACCEPT)
@@ -82,7 +83,7 @@ class ConnectionDialog(Gtk.Dialog):
         port -- Port number on the server
         """
         self.set_page(1)
-        self.connecting_page.message = "Connecting to {} ...".format(server)
+        self.connecting_page.message = _("Connecting to {} ...").format(server)
         self.connecting_page.deferred = widget.deferred
         
     def show_joining_document_page(self, widget, documentname):
@@ -94,7 +95,7 @@ class ConnectionDialog(Gtk.Dialog):
         documentname -- The name of the document we are opening
         """
         self.set_page(1)
-        self.connecting_page.message = "Opening {} ...".format(documentname)
+        self.connecting_page.message = _("Opening {} ...").format(documentname)
         self.connecting_page.deferred = widget.deferred
     
     def set_page(self, page):

cournal/connectiondialog/documentchooser.py
@@ -46,6 +46,7 @@ class DocumentChooser(Gtk.Box):
         self.dialog = dialog
         
         builder = Gtk.Builder()
+        builder.set_translation_domain("cournal")
         builder.add_from_file(cournal.__path__[0] + "/document_chooser.glade")
         self.main_grid = builder.get_object("main_grid")
         
@@ -116,7 +117,7 @@ class DocumentChooser(Gtk.Box):
         if len(documents) > 0:
             self.doc_name.set_text(documents[0])
         else:
-            self.doc_name.set_text("New document")
+            self.doc_name.set_text(_("New document"))
         
         self.emit("got_document_list")
 

cournal/connectiondialog/serverdetails.py
@@ -74,7 +74,7 @@ class ServerDetails(Gtk.Box):
         port = self._server_entry.port
         
         if port > 65535 or port < 0:
-            self.dialog.error = "The port must be below 65535"
+            self.dialog.error = _("The port must be below 65535")
             return
         
         if not self.dialog.parent.document.is_empty():
@@ -89,9 +89,9 @@ class ServerDetails(Gtk.Box):
         Ask the user, if he wishes to loose all changes he made to the current
         document, when he connects to a server.
         """
-        message = Gtk.MessageDialog(self.dialog, (Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT), Gtk.MessageType.WARNING, Gtk.ButtonsType.YES_NO, "Close current document?" )
-        message.format_secondary_text("You will loose all changes to your current document, if you connect to a server. Continue without saving?")
-        message.set_title("Warning")
+        message = Gtk.MessageDialog(self.dialog, (Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT), Gtk.MessageType.WARNING, Gtk.ButtonsType.YES_NO, _("Close current document?"))
+        message.format_secondary_text(_("You will loose all changes to your current document, if you connect to a server. Continue without saving?"))
+        message.set_title(_("Warning"))
         if message.run() != Gtk.ResponseType.YES:
             message.destroy()
             return False

cournal/document/document.py
@@ -52,7 +52,7 @@ class Document:
             self.height += page.height
             self.num_of_pages = len(self.pages)
         
-        print("The document has {} pages".format(self.num_of_pages))
+        print(_("The document has {} pages").format(self.num_of_pages))
         
     def is_empty(self):
         """
@@ -80,7 +80,7 @@ class Document:
         try:
             surface = cairo.PDFSurface(filename, 0, 0)
         except IOError as ex:
-            print("Error saving document:", ex)
+            print(_("Error saving document: {}").format(ex))
             #FIXME: Move error handler to mainwindow.py and show error message
             return
         
@@ -106,7 +106,7 @@ class Document:
         try:
             f = open_xoj(filename, "wb")
         except IOError as ex:
-            print("Error saving document:", ex)
+            print(_("Error saving document: {}").format(ex))
             #FIXME: Move error handler to mainwindow.py and show error message
             return
         

cournal/mainwindow.glade
@@ -44,7 +44,7 @@
           <object class="GtkMenuItem" id="menuitem1">
             <property name="use_action_appearance">False</property>
             <property name="can_focus">False</property>
-            <property name="label" translatable="yes">_Datei</property>
+            <property name="label" translatable="yes">_File</property>
             <property name="use_underline">True</property>
             <child type="submenu">
               <object class="GtkMenu" id="menu1">
@@ -62,7 +62,7 @@
                 </child>
                 <child>
                   <object class="GtkImageMenuItem" id="imagemenuitem_open_pdf">
-                    <property name="label">Annotate PDF</property>
+                    <property name="label" translatable="yes">Annotate PDF</property>
                     <property name="use_action_appearance">False</property>
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
@@ -72,7 +72,7 @@
                 </child>
                 <child>
                   <object class="GtkImageMenuItem" id="imagemenuitem_connect">
-                    <property name="label">Connect to Server</property>
+                    <property name="label" translatable="yes">Connect to Server</property>
                     <property name="use_action_appearance">False</property>
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
@@ -109,7 +109,7 @@
                 </child>
                 <child>
                   <object class="GtkImageMenuItem" id="imagemenuitem_export_pdf">
-                    <property name="label">Export PDF</property>
+                    <property name="label" translatable="yes">Export PDF</property>
                     <property name="use_action_appearance">False</property>
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
@@ -119,7 +119,7 @@
                 </child>
                 <child>
                   <object class="GtkImageMenuItem" id="imagemenuitem_import_xoj">
-                    <property name="label">Import</property>
+                    <property name="label" translatable="yes">Import</property>
                     <property name="use_action_appearance">False</property>
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
@@ -149,75 +149,11 @@
           </object>
         </child>
         <child>
-          <object class="GtkMenuItem" id="menuitem2">
-            <property name="use_action_appearance">False</property>
-            <property name="can_focus">False</property>
-            <property name="no_show_all">True</property>
-            <property name="label" translatable="yes">_Bearbeiten</property>
-            <property name="use_underline">True</property>
-            <child type="submenu">
-              <object class="GtkMenu" id="menu2">
-                <property name="visible">True</property>
-                <property name="can_focus">False</property>
-                <child>
-                  <object class="GtkImageMenuItem" id="imagemenuitem6">
-                    <property name="label">gtk-cut</property>
-                    <property name="use_action_appearance">False</property>
-                    <property name="can_focus">False</property>
-                    <property name="no_show_all">True</property>
-                    <property name="use_underline">True</property>
-                    <property name="use_stock">True</property>
-                  </object>
-                </child>
-                <child>
-                  <object class="GtkImageMenuItem" id="imagemenuitem7">
-                    <property name="label">gtk-copy</property>
-                    <property name="use_action_appearance">False</property>
-                    <property name="can_focus">False</property>
-                    <property name="no_show_all">True</property>
-                    <property name="use_underline">True</property>
-                    <property name="use_stock">True</property>
-                  </object>
-                </child>
-                <child>
-                  <object class="GtkImageMenuItem" id="imagemenuitem8">
-                    <property name="label">gtk-paste</property>
-                    <property name="use_action_appearance">False</property>
-                    <property name="can_focus">False</property>
-                    <property name="no_show_all">True</property>
-                    <property name="use_underline">True</property>
-                    <property name="use_stock">True</property>
-                  </object>
-                </child>
-                <child>
-                  <object class="GtkImageMenuItem" id="imagemenuitem9">
-                    <property name="label">gtk-delete</property>
-                    <property name="use_action_appearance">False</property>
-                    <property name="can_focus">False</property>
-                    <property name="no_show_all">True</property>
-                    <property name="use_underline">True</property>
-                    <property name="use_stock">True</property>
-                  </object>
-                </child>
-              </object>
-            </child>
-          </object>
-        </child>
-        <child>
-          <object class="GtkMenuItem" id="menuitem3">
-            <property name="use_action_appearance">False</property>
-            <property name="can_focus">False</property>
-            <property name="no_show_all">True</property>
-            <property name="label" translatable="yes">_Ansicht</property>
-            <property name="use_underline">True</property>
-          </object>
-        </child>
-        <child>
           <object class="GtkMenuItem" id="menuitem4">
             <property name="use_action_appearance">False</property>
             <property name="visible">True</property>
             <property name="can_focus">False</property>
-            <property name="label" translatable="yes">_Hilfe</property>
+            <property name="label" translatable="yes">_Help</property>
             <property name="use_underline">True</property>
             <child type="submenu">
               <object class="GtkMenu" id="menu3">

commit 067166e0a8cf048b338578f3d0c628616a545eaf

Fix the server
Was broken since cd375c3428f2882879f6698317ff66e3971546b5

cournal-server.py
@@ -26,5 +26,4 @@ class Page(server.Page):
     pass
 
 if __name__ == "__main__":
-    print(sys.path)
     sys.exit(server.main())

cournal/server/server.py
@@ -18,6 +18,7 @@
 # along with Cournal.  If not, see <http://www.gnu.org/licenses/>.
 
 import os, sys
+import gettext
 from io import StringIO
 import string
 import atexit
@@ -486,6 +487,10 @@ def docname_to_filename(name):
 
 def main():
     """Start a Cournal server"""
+    locale_dir = os.path.join(sys.prefix, "local",  "share", "locale")
+    #locale_dir = os.path.realpath(os.path.dirname(sys.argv[0]))
+    gettext.install("cournal") #, locale_dir)
+
     args = CmdlineParser().parse()
     port = args.port
     