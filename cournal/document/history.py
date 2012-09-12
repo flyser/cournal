0f7b98ad3a7f34a91f81f147af80c38ef6ffba48
Fabian Henze <flyser42@gmx.de>
2012-09-13 01:14:49 +0200

    Port the toolbar, the menu bar and the accelerators to GtkAction
    Sadly this will only work with Gtk 3.6 without errors. I am trying
    to convice the developer of the bugfix to backport it to 3.4.5
    
    https://bugzilla.gnome.org/show_bug.cgi?id=671786

--- cournal/document/history.py
+++ cournal/document/history.py
@@ -18,49 +18,44 @@
 # along with Cournal.  If not, see <http://www.gnu.org/licenses/>.
 
 from cournal.network import network
-
-def reset():
-    """Reset undo history."""
-    global _undo_list, _redo_list
-    deactivate_undo()
-    deactivate_redo()
-    _undo_list = []
-    _redo_list = []
     
-
-def init(menu_undo, menu_redo, tool_undo, tool_redo):
+def init(undo_action, redo_action):
     """
     Initialize the undo history.
     
     Positional arguments:
-    menu_undo -- undo widget in the menubar
-    menu_redo -- redo widget in the menubar
-    tool_undo -- undo widget in the toolbar
-    tool_redo -- redo widget in the toolbar
+    undo_action -- Gtk.Action for undo
+    redo_action -- Gtk.Action for redo
     """
-    global _menu_undo, _menu_redo, _tool_redo, _tool_undo, _undo_list, _redo_list
-    _menu_undo = menu_undo
-    _menu_redo = menu_redo
-    _tool_undo = tool_undo
-    _tool_redo = tool_redo
+    global _undo_action, _redo_action, _undo_list, _redo_list
+    _undo_action = undo_action
+    _redo_action = redo_action
     _undo_list = []
     _redo_list = []
     
-def undo(menuitem):
+def reset():
+    """Reset undo history."""
+    global _undo_list, _redo_list
+    _undo_action.set_sensitive(False)
+    _redo_action.set_sensitive(False)
+    _undo_list = []
+    _redo_list = []
+
+def undo(action):
     """Undo last command."""
     command = _undo_list.pop()
     add_redo_command(command)
     command.undo()
     if len(_undo_list) == 0:
-        deactivate_undo()
+        _undo_action.set_sensitive(False)
 
-def redo(menuitem):
+def redo(action):
     """Redo undone command."""
     command = _redo_list.pop()
     add_undo_command(command, clear_redo=False)
     command.redo()
     if len(_redo_list) == 0:
-        deactivate_redo()
+        _redo_action.set_sensitive(False)
 
 def register_draw_stroke(stroke, page):
     """
@@ -97,10 +92,10 @@ def add_undo_command(command, clear_redo=True):
     if len(_undo_list) > 20:
         _undo_list.pop(0)
     if len(_undo_list) == 1:
-        activate_undo()
+        _undo_action.set_sensitive(True)
     if clear_redo:
         _redo_list = []
-        deactivate_redo()
+        _redo_action.set_sensitive(False)
 
 def add_redo_command(command):
     """
@@ -111,27 +106,7 @@ def add_redo_command(command):
     """
     _redo_list.append(command)
     if len(_redo_list) == 1:
-        activate_redo()
-
-def deactivate_undo():
-    """Deactivate undo buttons."""
-    _menu_undo.set_sensitive(False)
-    _tool_undo.set_sensitive(False)
-
-def deactivate_redo():
-    """Deactivate redo buttons."""
-    _menu_redo.set_sensitive(False)
-    _tool_redo.set_sensitive(False)
-
-def activate_undo():
-    """Activate undo buttons."""
-    _menu_undo.set_sensitive(True)
-    _tool_undo.set_sensitive(True)
-
-def activate_redo():
-    """Activate redo buttons."""
-    _menu_redo.set_sensitive(True)
-    _tool_redo.set_sensitive(True)
+        _redo_action.set_sensitive(True)
 
 class CommandDrawStroke:
     """Draw stroke command."""
